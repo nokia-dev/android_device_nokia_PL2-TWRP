a0a05e2a985e1ba934d6254fc6b7d3a4bf5c4b21
theimpulson <aayushgupta219@gmail.com>
2018-12-19 07:43:42 +0000

    PL2: Get ready for Pie?
    Change-Id: I02ec6be37a6454581e6e243c01fa2a1b14f2ea9d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,7 +24,7 @@ TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := kryo
 
 TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_ARCH_VARIANT := armv8-a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
@@ -36,25 +36,20 @@ TARGET_BOARD_PLATFORM_GPU := qcom-adreno508
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := sdm660
 TARGET_NO_BOOTLOADER := true
-TARGET_USES_UEFI := true
 
-# Crypto
-TARGET_HW_DISK_ENCRYPTION := true
-
-# Enable CPUSets
-ENABLE_CPUSETS := true
-ENABLE_SCHEDBOOST := true
+# Use system's VOLD and files in case TWRP's decryption fails
+TW_CRYPTO_USE_SYSTEM_VOLD := qseecomd
 
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 earlycon=msm_serial_dm,0xc170000 androidboot.hardware=qcom msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 service_locator.enable=1 swiotlb=1 androidboot.configfs=true androidboot.usbcontroller=a800000.dwc3
-BOARD_KERNEL_CMDLINE += skip_override androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE     += skip_override androidboot.selinux=permissive
 BOARD_KERNEL_BASE        := 0x00000000
-BOARD_KERNEL_OFFSET := 0x00008000
+BOARD_KERNEL_OFFSET      := 0x00008000
 BOARD_KERNEL_PAGESIZE    := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET     := 0x01000000
-BOARD_SECOND_OFFSET := 0x00f00000
-TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/Image.gz-dtb
+BOARD_SECOND_OFFSET      := 0x00f00000
+TARGET_PREBUILT_KERNEL   := $(LOCAL_PATH)/Image.gz-dtb
 
 # Partitions
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
@@ -62,32 +57,30 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 
 BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
-BOARD_ROOT_EXTRA_FOLDERS := bt_firmware dsp firmware persist
 BOARD_USES_RECOVERY_AS_BOOT := true
 BOARD_HAS_REMOVABLE_STORAGE := true
-
 BOARD_SUPPRESS_SECURE_ERASE := true
 
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 
 # Recovery
-TARGET_RECOVERY_WIPE := $(LOCAL_PATH)/recovery/root/etc/recovery.wipe
-
 BOARD_HAS_LARGE_FILESYSTEM := true
 BOARD_HAS_NO_SELECT_BUTTON := true
+
+# Security Patch Hack
+PLATFORM_SECURITY_PATCH := 2025-12-31
 
 # Vendor
 TARGET_COPY_OUT_VENDOR := vendor
 
 # TWRP Configuration
-AB_OTA_UPDATER := true
 TW_THEME := portrait_hdpi
+TW_USE_TOOLBOX := true
 TW_INCLUDE_CRYPTO := true
 TW_MAX_BRIGHTNESS := 100
 TW_DEFAULT_BRIGHTNESS := "30"
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
-TW_EXCLUDE_SUPERSU := true
 TW_EXTRA_LANGUAGES := true
 TW_NO_SCREEN_BLANK := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
@@ -95,6 +88,3 @@ TARGET_RECOVERY_QCOM_RTC_FIX := true
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 RECOVERY_SDCARD_ON_DATA := true
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
-
-TARGET_RECOVERY_DEVICE_MODULES += android.hardware.boot@1.0
-TW_RECOVERY_ADDITIONAL_RELINK_FILES := ${OUT}/system/lib64/android.hardware.boot@1.0.so
--- omni_PL2.mk
+++ omni_PL2.mk
@@ -17,13 +17,53 @@
 # Release name
 PRODUCT_RELEASE_NAME := PL2
 
+# Inherit Build configuration for a very minimal build of the Open-Source part of the tree.
 $(call inherit-product, build/target/product/embedded.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
-# Inherit language packages
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+# AB update support
+AB_OTA_UPDATER := true
+
+AB_OTA_PARTITIONS += \
+    boot \
+    system \
+    vendor
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+PRODUCT_PACKAGES += \
+    otapreopt_script
+
+# Boot control
+PRODUCT_PACKAGES_DEBUG += \
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-service \
+    bootctl
+
+# Update engine
+PRODUCT_PACKAGES += \
+    brillo_update_payload \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
+
+PRODUCT_PACKAGES += \
+    bootctrl.sdm660
+
+PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+    bootctrl.sdm660 \
+    libcutils \
+    libgptutils \
+    libz \
+
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
 
 # Charger
 PRODUCT_PACKAGES += \
@@ -43,9 +83,13 @@ PRODUCT_COPY_FILES += \
     $(TZDATAPATH)/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
 endif
 
+# Keystore
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.keystore=sdm660
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := omni_PL2
 PRODUCT_DEVICE := PL2
+PRODUCT_MANUFACTURER := FIH
 PRODUCT_BRAND := Nokia
-PRODUCT_MODEL := 6.1
-PRODUCT_MANUFACTURER := Nokia
+PRODUCT_MODEL := Nokia 6.1
