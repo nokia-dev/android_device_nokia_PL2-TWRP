aeee217ee74a38932848cc2263dac0492a8fa3fc
theimpulson <aayushgupta219@gmail.com>
2019-07-01 13:02:20 +0000

    PL2: Get rid of TWRP app
    *Depends upon https://gerrit.omnirom.org/#/c/android_bootable_recovery/+/33841/

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -87,6 +87,7 @@ TW_THEME := portrait_hdpi
 TW_USE_TOOLBOX := true
 TW_INCLUDE_REPACKTOOLS := true
 TW_HAS_EDL_MODE := true
+TW_EXCLUDE_TWRPAPP := true
 
 # Workaround for error copying vendor files to recovery ramdisk
 TARGET_COPY_OUT_VENDOR := system/vendor
